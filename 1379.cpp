3462c5bcbabd580181ec66313b325d9738d88064
Yue Zhihan <zhihan.yue@foxmail.com>
2017-04-23 12:03:18 +0800

    AC_2016MS_220K

--- 1379.cpp
+++ 1379.cpp
@@ -1,64 +1,68 @@
 #include <cstdio>
+#include <cstring>
+#include <algorithm>
 #include <cmath>
-#include <vector>
+#include <string>
 #include <cstdlib>
 #include <ctime> 
-#include <climits>
-#include <algorithm>
 using namespace std;
-#define fromto(from,to,i) for(int i=(from);i<=(to);++i)
-#define MAXN 1008
-#define MAXDIS 15000
-#define EPS 1e-5
-#define INF 1e20
-#define PI 3.1415926535
+#define N 1008
 #define CNTP 30
 #define CNTC 30
-typedef pair<double,double> Point;
-Point P[MAXN]; 
-int n,mx,my;
-
+#define EPS 1e-8
+#define INF 1e20
+#define MAXDIS 15000
+#define PI 3.1415926
+struct POINT {
+    double x, y, d;
+    POINT() {}
+    POINT(double _x, double _y): x(_x), y(_y) {}
+} p[N], rp[CNTP];
 double myrand(double L,double R) {
     return L+(R-L)*rand()/RAND_MAX;
 }
-double pow2(double x){return x*x;}
-double mindist(Point x) {
-    double dist=INF;
-    fromto(1,n,i) dist=min(dist,sqrt(pow2(x.first-P[i].first)+pow2(x.second-P[i].second)));
-    return dist;
-}
-Point solve()
-{
-    static Point Sta[CNTP+1];static double D[CNTP+1];
-    fromto(1,CNTP,i){Sta[i]=Point(myrand(0,mx),myrand(0,my));D[i]=mindist(Sta[i]);}
-    
-    double T=MAXDIS;
-    while(T>=EPS) {
-        fromto(1,CNTP,i) fromto(1,CNTC,kkk) {
-            double jiao=myrand(0,10*PI);
-            Point newp(Sta[i].first+T*cos(jiao),Sta[i].second+T*sin(jiao));
-            if(newp.first<0 || newp.first>mx || newp.second<0 || newp.second>my) continue;
-            double newval=mindist(newp),delta=newval-D[i];
-            if(delta>0){D[i]=newval;Sta[i]=newp;}
-            //if(delta>0 || myrand(0,1)<=exp(delta/T)){D[i]=newval;Sta[i]=newp;}
-        }
-        T*=0.8;
-    }
-    int maxi=1;
-    fromto(1,CNTP,i) if(D[maxi]<D[i]) maxi=i;
-    return Sta[maxi];
+int x, y, n;
+double dis(POINT a) {
+    double ret=INF;
+    for (int i=0; i<n; i++)
+        ret=min(ret,sqrt((a.x-p[i].x)*(a.x-p[i].x) + (a.y-p[i].y)*(a.y-p[i].y)));
+    return ret;
 }
 
-int main()
-{
+POINT solve() {
+    for (int i=0; i<CNTP; i++) {
+        rp[i]= POINT(myrand(0,x),myrand(0,y));
+        rp[i].d = dis(rp[i]);
+    }
+    double T=MAXDIS;
+    while (T>=EPS) {
+        for (int i=0; i<CNTP; i++) {
+            for (int k=0; k<CNTC; k++) {
+                double jiao=myrand(0,10*PI);
+                POINT t(rp[i].x+T*cos(jiao),rp[i].y+T*sin(jiao));
+                t.d = dis(t);
+                if (0<=t.x && t.x<=x && 0<=t.y && t.y<=y)
+                    if (t.d > rp[i].d) rp[i] = t;
+            }
+        }
+        T *= 0.8;
+    }
+    int k = 0;
+    for (int i=1; i<CNTP; i++)
+        if (rp[k].d < rp[i].d) k = i;
+    return rp[k];
+}
+
+int main() {
     srand(time(0));
-    int case_cnt;
-    scanf("%d",&case_cnt);
-    while(case_cnt--) {
-        scanf("%d%d%d",&mx,&my,&n);
-        fromto(1,n,i) scanf("%lf%lf",&P[i].first,&P[i].second);
-        Point ans=solve();
-        printf("The safest point is (%.1lf, %.1lf).\n",ans.first,ans.second);
+    int cas;
+    scanf("%d", &cas);
+    while (cas--) {
+        scanf("%d%d%d", &x, &y, &n);
+        for (int i=0; i<n; i++)
+            scanf("%lf %lf", &p[i].x, &p[i].y);
+        POINT ans=solve();
+        printf("The safest point is (%.1lf, %.1lf).\n",ans.x,ans.y);
     }
     return 0;
 }
