9f18ee70001463e0f663e4874ac23ae74a233a47
Yue Zhihan <zhihan.yue@foxmail.com>
2017-04-26 12:07:32 +0800

    MLE

--- 1611.cpp
+++ 1611.cpp
@@ -18,14 +18,14 @@ void merge(int L1,int L2) {
     //if(L[L1].size()>L[L2].size()) swap(L1,L2);
     foreach(L[L1],p,vector<int>) inL[*p]=L2;
     L[L2].insert(L[L2].end(),L[L1].begin(),L[L1].end());
-    L[L1].erase(L[L1].begin(),L[L1].end());
+    L[L1]=vector<int>();
 }
 int main()
 {
     while(true) {
         int n,m;scanf("%d%d",&n,&m);
         if(n==0 && m==0) break;
-        fromto(0,n-1,i){inL[i]=i;L[i].clear();L[i].push_back(i);}
+        fromto(0,n-1,i){inL[i]=i;L[i]=vector<int>();L[i].push_back(i);}
         while(m--) {
             int k,s0,s;scanf("%d%d",&k,&s0);
             fromto(2,k,j) {
