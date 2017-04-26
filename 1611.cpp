43604558a676f87a8a9fff41ebfd720c621746cb
Yue Zhihan <zhihan.yue@foxmail.com>
2017-04-26 12:07:03 +0800

    AC

--- 1611.cpp
+++ 1611.cpp
@@ -12,21 +12,19 @@ using namespace std;
 #define INF INT_MAX
 #define MAXN 30008
 vector<int> L[MAXN];
-int L_size[MAXN];
 int inL[MAXN];
 void merge(int L1,int L2) {
     if(L1==L2) return;
-    if(L_size[L1]>L_size[L2]) swap(L1,L2);
+    if(L[L1].size()>L[L2].size()) swap(L1,L2);
     foreach(L[L1],p,vector<int>) inL[*p]=L2;
     L[L2].insert(L[L2].end(),L[L1].begin(),L[L1].end());
-    L_size[L2]+=L_size[L1];L_size[L1]=0;
 }
 int main()
 {
     while(true) {
         int n,m;scanf("%d%d",&n,&m);
         if(n==0 && m==0) break;
-        fromto(0,n-1,i){inL[i]=i;L[i].clear();L[i].push_back(i);L_size[i]=1;}
+        fromto(0,n-1,i){inL[i]=i;L[i].clear();L[i].push_back(i);}
         while(m--) {
             int k,s0,s;scanf("%d%d",&k,&s0);
             fromto(2,k,j) {
@@ -34,7 +32,7 @@ int main()
                 merge(inL[s0],inL[s]);
             }
         }
-        printf("%d\n",L_size[inL[0]]);
+        printf("%d\n",L[inL[0]].size());
     }
     return 0;
 }
