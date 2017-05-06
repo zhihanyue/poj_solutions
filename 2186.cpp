4c03ca00211a1065456f289cd5b03fbf868e3d99
Yue Zhihan <zhihan.yue@foxmail.com>
2017-05-06 12:20:54 +0800

    WA

--- 2186.cpp
+++ 2186.cpp
@@ -1,94 +1,75 @@
-/*
-kosaraju
-Memory 1036K
-Time    63MS
-*/
-#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#define fromto(from,to,i) for(int (i)=(from);(i)<=(to);++(i))
 using namespace std;
-#define MAXM 50010
-#define MAXV 10010
-#define min(a,b) (a>b?b:a)
 
-typedef struct{
-    int s,t,next,next2;
-}Edge;
+vector<int> G[40008],rG[40008];
+int T[40008],T_len;
+int SCC[40008];
+bool vis[40008];
 
-Edge edge[MAXM];
-int n,m,headlist[MAXV],headlist2[MAXV];
-
-int order[MAXV],belong[MAXV];
-int num,count;
-bool vis[MAXV];
-
-void dfs(int x){
-    int i,a;
-    vis[x]=1;
-    for(i=headlist[x];i!=-1;i=edge[i].next){
-        a=edge[i].t;
-        if(!vis[a]) dfs(a);
+void dfs(int u)
+{
+    vis[u]=true;
+    for(int i=0,size=G[u].size();i<size;++i) {
+        int v=G[u][i];
+        if(!vis[v]) dfs(v);
     }
-    order[++num]=x;
+    T[++T_len]=u;
 }
 
-void dfst(int x){
-    int i,a;
-    belong[x]=count;        //记录结点属于哪个连通分量
-    vis[x]=1;
-    for(i=headlist2[x];i!=-1;i=edge[i].next2){      //要将边反过来遍历一遍
-        a=edge[i].s;
-        if(!vis[a]) dfst(a);
+void rdfs2(int u)
+{
+    vis[u]=true;
+    for(int i=0,size=rG[u].size();i<size;++i) {
+        int v=rG[u][i];
+        if(!vis[v]) rdfs2(v);
     }
+    T[++T_len]=u;
 }
 
-void kosaraju(){
-    int i;
-    memset(vis,0,sizeof(vis));
-    num=count=0;
-    for(i=1;i<=n;i++)
-        if(!vis[i]) dfs(i);
-    memset(vis,0,sizeof(vis));
-        
-    for(i=n;i>=1;i--)
-        if(!vis[order[i]]){
-            count++;
-            dfst(order[i]);
-        }
-}
-
-void output(){
-    int i,outdegree[MAXV]={0},outzero=0,ans=0,cnt;
-    for(i=0;i<m;i++){
-        if(belong[edge[i].s]!=belong[edge[i].t]){
-            outdegree[belong[edge[i].s]]++;
-        }
+void rdfs(int u,int k)
+{
+    SCC[u]=k;
+    for(int i=0,size=rG[u].size();i<size;++i) {
+        int v=rG[u][i];
+        if(SCC[v]==-1) rdfs(v,k);
     }
-    for(i=1;i<=count;i++){
-        if(!outdegree[i]){
-            outzero++;
-            cnt=i;
-        }
-    }
-    for(i=1;i<=n;i++)
-        if(belong[i]==cnt) ans++;
-        if(outzero==1)  printf("%d\n",ans);
-        else printf("0\n");
-}
+}       
 
-int main(){
-    int i,a,b;
-    scanf("%d%d",&n,&m);
-        memset(headlist,-1,sizeof(headlist));
-        memset(headlist2,-1,sizeof(headlist2));
-        for(i=0;i<m;i++){
-            scanf("%d%d",&a,&b);
-            edge[i].s=a;
-            edge[i].t=b;
-            edge[i].next=headlist[a];
-            headlist[a]=i;
-            edge[i].next2=headlist2[b];     //记录反边
-            headlist2[b]=i;
-        }
-        kosaraju();
-        output();
+int main()
+{
+    int N,M;
+    while(scanf("%d%d",&N,&M)==2) {
+    fromto(1,N,i) {
+        G[i].clear();
+        rG[i].clear();
+    }
+    fromto(1,M,i) {
+        int u,v;
+        scanf("%d%d",&u,&v);
+        G[u].push_back(v);
+        rG[v].push_back(u);
+    }
+    memset(vis,false,sizeof(vis));
+    T_len=0;
+    fromto(1,N,i) if(!vis[i]) dfs(i);
+    reverse(T+1,T+T_len+1);
+    
+    memset(SCC,-1,sizeof(SCC));
+    int k=0;
+    fromto(1,T_len,i)
+        if(SCC[T[i]]==-1) rdfs(T[i],++k);
+    
+    memset(vis,false,sizeof(vis));
+    int temp=T[T_len];
+    T_len=0;
+    rdfs2(temp);
+    int res=0;
+    if(T_len==N) fromto(1,N,i)
+        if(SCC[i]==k) ++res;
+    printf("%d\n",res);
+    }
     return 0;
 }
