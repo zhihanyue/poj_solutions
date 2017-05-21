74476dd5b08530bf2d2be13d7c8ceea3161de768
Yue Zhihan <zhihan.yue@foxmail.com>
2017-05-21 12:38:49 +0800

    re

--- 3481.cpp
+++ 3481.cpp
@@ -1,19 +1,125 @@
 #include <cstdio>
-#include <set>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
 using namespace std;
 
-struct Node { int K,P; };
-struct cmp
+struct T_val
 {
-    bool operator()(Node a,Node b)
+    int K,P;
+    bool operator==(const T_val &other) const
     {
-        return a.P<b.P;
+        return P==other.P;
+    }
+    bool operator>(const T_val &other) const
+    {
+        return P>other.P;
     }
 };
-multiset<Node,cmp> myset;
+
+struct Node
+{
+    Node *c[2];
+    T_val val;
+    int pri;
+    int cnt;
+    Node(T_val v)
+    {
+        c[0]=c[1]=0;
+        cnt=1;
+        val=v;
+        pri=rand();
+    }
+    int cmp(T_val x) const
+    {
+        return x==val?-1:(x>val);
+    }
+};
+Node *root=0;
+
+void rotate(Node *&o,bool w)
+{
+    Node *k=o->c[!w];
+    o->c[!w]=k->c[w];
+    k->c[w]=o;
+    o=k;
+}
+
+void insert(Node *&o,T_val v)
+{
+    if(o==0)
+        o=new Node(v);
+    else
+    {
+        int w=o->cmp(v);
+        if(w==-1)
+            ++o->cnt;
+        else
+        {
+            insert(o->c[w],v);
+            if(o->pri < o->c[w]->pri)
+                rotate(o,!w);
+        }
+    }
+}
+
+void remove(Node *&o,T_val v)
+{
+    if(o==0)
+        return;
+    int w=o->cmp(v);
+    if(w!=-1)
+        return remove(o->c[w],v);
+    if(o->cnt>1)
+        --o->cnt;
+    else if(o->c[0]==0)
+    {
+        Node *k=o->c[1];
+        delete o;
+        o=k;
+    }
+    else if(o->c[1]==0)
+    {
+        Node *k=o->c[0];
+        delete o;
+        o=k;
+    }
+    else
+    {
+        int w=o->c[0]->pri > o->c[1]->pri;
+        rotate(o,w);
+        remove(o->c[w],v);
+    }
+}
+
+Node* find(Node *o,T_val v)
+{
+    if(o==0)
+        return 0;
+    int w=o->cmp(v);
+    if(w==-1)
+        return o;
+    else return find(o->c[w],v);
+}
+
+Node* t_m(Node *o,int w)
+{
+    while(o->c[w])
+        o=o->c[w];
+    return o;
+}
 
 int main()
 {
+/*  insert(root,453);
+    insert(root,78);
+    insert(root,521);
+    insert(root,46853);
+    printf("%d\n",t_m(root,1)->val);
+    remove(root,46853);
+    printf("%d\n",t_m(root,1)->val);
+    system("pause");*/
+    srand(time(0));
     while(true)
     {
         int N;
@@ -24,28 +130,29 @@ int main()
         {
             int K,P;
             scanf("%d%d",&K,&P);
-            myset.insert((Node){K,P});
+            insert(root,(T_val){K,P});
         }
         else if(N==2)
         {
-            if(myset.size()!=0)
+            if(root==0)
+                printf("0\n");
+            else
             {
-                set<Node>::iterator it=myset.end();
-                --it;
-                printf("%d\n",it->K);
-                myset.erase(it);
+                T_val m=t_m(root,1)->val;
+                printf("%d\n",m.K);
+                remove(root,m);
             }
-            else printf("0\n");
         }
         else if(N==3)
         {
-            if(myset.size()!=0)
+            if(root==0)
+                printf("0\n");
+            else
             {
-                set<Node>::iterator it=myset.begin();
-                printf("%d\n",it->K);
-                myset.erase(it);
+                T_val m=t_m(root,0)->val;
+                printf("%d\n",m.K);
+                remove(root,m);
             }
-            else printf("0\n");
         }
     }
     return 0;
