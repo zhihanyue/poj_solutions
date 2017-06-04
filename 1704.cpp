12fe18174ca5bc9f42b797faca27891c11da581e
Yue Zhihan <zhihan.yue@foxmail.com>
2017-06-04 13:02:04 +0800

    WA

---
+++ 1704.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int a[1008];
+int main()
+{
+    int T;
+    scanf("%d",&T);
+    while(T--) {
+        int n;
+        scanf("%d",&n);
+        for(int i=0;i<n;++i) scanf("%d",&a[i]);
+        if(n%2!=0) a[n++]=0;
+        sort(a,a+n);
+        int x=0;
+        for(int i=1;i<n;++i) x^=a[i]-a[i-1]-1;
+        printf(x?"Georgia will win\n":"Bob will win\n");
+    }
+    return 0;
+}
