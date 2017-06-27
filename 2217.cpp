949fbf3e7186c91f9b0244869cf752377654a4e1
Yue Zhihan <zhihan.yue@foxmail.com>
2017-06-27 13:16:19 +0800

    AC 360ms 8064k

--- 2217.cpp
+++ 2217.cpp
@@ -9,7 +9,6 @@ using namespace std;
 #define watch(a,size) for(int __i=0;__i<(size);++__i) printf("[%d]=%d ",__i,(a)[__i]);printf("\n");
 typedef pair<int,int> sta;
 char A[2*MAXN],B[MAXN];
-int fA_len;
 
 int sa[MAXN],rank[2*MAXN],lcp[MAXN],sa_k;
 
@@ -18,7 +17,7 @@ bool cmp(int x,int y)
     return sta(rank[x],rank[x+sa_k])<sta(rank[y],rank[y+sa_k]);
 }
 
-void getsa(char *a,int n)//->sa,rank
+void getsa(char *a,int n)
 {
     memset(rank,-1,sizeof(rank));
     fromto(1,n,i) {sa[i]=i;rank[i]=a[i];}
@@ -31,25 +30,25 @@ void getsa(char *a,int n)//->sa,rank
     }
 }
 
-void getlcp(char *a,int n)//sa,rank->lcp
+void getlcp(char *a,int n)
 {
     int h=0;
-    fromto(1,n,i) rank[sa[i]]=i;//!!!
     fromto(1,n,i) {
-        if(rank[i]<=1) continue;
+        if(rank[i]<=1) continue;//!!!
         if(h>0) --h;
         int j=sa[rank[i]-1];
-        while(i+h<=n && j+h<=n && a[i+h]==a[j+h]) ++h;//!!!
+        while(a[i+h]==a[j+h]) ++h;
         lcp[rank[i]]=h;
     }
 }
 
 int main()
 {
-    int casecnt; scanf("%d",&casecnt); getchar();
+    int casecnt;
+    scanf("%d\n",&casecnt);
     while(casecnt--) {
         gets(A+1);gets(B+1);
-        fA_len=strlen(A+1);
+        int fA_len=strlen(A+1);
         B[0]='$';
         strcat(A+1,B);
         int A_len=strlen(A+1);
