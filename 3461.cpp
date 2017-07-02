8013fd6592a63d580feb804c3bec93f00d9f08f1
Yue Zhihan <zhihan.yue@foxmail.com>
2017-07-02 13:18:08 +0800

    WA

--- 3461.cpp
+++ 3461.cpp
@@ -8,26 +8,33 @@ int f[10000];
 int KMP(char P[],char T[])
 {
     int cnt=0;
-    int n=strlen(P+1)+1,m=strlen(T+1)+1,j=0;
-    for(int i=1;i<=n;++i) {
-        while(j>0 && T[j+1]!=P[i]) j=f[j];
-        if(T[j+1]==P[i]) ++j;
-        //此时的j表示以i位置为尾的最大匹配长度 
-        if(j==m-1) ++cnt;
+    int n=strlen(P+1),m=strlen(T+1),j=0;
+    for(int i=1;i<=n;++i)
+    {
+        while(j>0 && T[j+1]!=P[i])
+            j=f[j];
+        if(T[j+1]==P[i])
+            ++j;
+        if(j==m)
+        {
+            ++cnt;
+            j=f[j];
+        }
     }
     return cnt;
 }
 
 void getfail(char T[])
 {
-    int m=strlen(T+1)+1,j=0;
+    int m=strlen(T+1),j=0;
     f[1]=0;
-    for(int i=2;i<=m;++i) {//错位匹配法，不能计算后缀为整个串的情况 
+    for(int i=2;i<=m;++i)
+    {
         while(j>0 && T[j+1]!=T[i])
             j=f[j];
         if(T[j+1]==T[i])
             ++j;
-        f[i]=j;//记录每一次失配的正确转移位置 
+        f[i]=j;
     }
 }
 
