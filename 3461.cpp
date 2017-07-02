10a0c1f6e2ffb41479cb893129ad548cad1112e4
Yue Zhihan <zhihan.yue@foxmail.com>
2017-07-02 13:18:30 +0800

    AC 110ms

--- 3461.cpp
+++ 3461.cpp
@@ -9,32 +9,24 @@ int KMP(char P[],char T[])
 {
     int cnt=0;
     int n=strlen(P+1),m=strlen(T+1),j=0;
-    for(int i=1;i<=n;++i)
-    {
-        while(j>0 && T[j+1]!=P[i])
-            j=f[j];
-        if(T[j+1]==P[i])
-            ++j;
-        if(j==m)
-        {
-            ++cnt;
-            j=f[j];
-        }
+    for(int i=1;i<=n;++i) {
+        while(j>0 && T[j+1]!=P[i]) j=f[j];//j>0 && T[j+1]!=P[i]等价于失败 
+        if(T[j+1]==P[i]) ++j;
+        if(j==m) ++cnt;
     }
     return cnt;
 }
 
 void getfail(char T[])
 {
-    int m=strlen(T+1),j=0;
-    f[1]=0;
-    for(int i=2;i<=m;++i)
+    int m=strlen(T+1);
+    f[0]=0;f[1]=0;
+    for(int i=1;i<m;++i)
     {
-        while(j>0 && T[j+1]!=T[i])
-            j=f[j];
-        if(T[j+1]==T[i])
-            ++j;
-        f[i]=j;
+        int j=f[i];
+        while(j>0 && T[j+1]!=T[i+1]) j=f[j];
+        f[i+1]=(T[j+1]==T[i+1])?j+1:0;
+        //f[i+1]=(j==0)?0:j+1; 这个没考虑在0处匹配失败的情况，是错误的写法！！！ 
     }
 }
 
