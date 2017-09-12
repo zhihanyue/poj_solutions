d1c66624d1d19530c7a204f1d63b69e2d3064ab5
Yue Zhihan <zhihan.yue@foxmail.com>
2017-09-12 14:20:18 +0800

    AC 7063ms

--- 2104.cpp
+++ 2104.cpp
@@ -21,14 +21,15 @@ void init(int o,int L,int r)
     merge(xdtree[o*2].begin(),xdtree[o*2].end(),xdtree[o*2+1].begin(),xdtree[o*2+1].end(),xdtree[o].begin());
 }
 
-int qu_ans,qu_L,qu_r,qu_x;
+int qu_low,qu_upp,qu_L,qu_r,qu_x;
 void query(int o,int L,int r)
 {
     if(r<=qu_L || L>=qu_r)
         return;
     if(qu_L<=L && r<=qu_r)
     {
-        qu_ans+=lower_bound(xdtree[o].begin(),xdtree[o].end(),qu_x)-xdtree[o].begin();
+        qu_low+=lower_bound(xdtree[o].begin(),xdtree[o].end(),qu_x)-xdtree[o].begin();
+        qu_upp+=upper_bound(xdtree[o].begin(),xdtree[o].end(),qu_x)-xdtree[o].begin();
         return;
     }
     int M=L+(r-L)/2;
@@ -55,16 +56,22 @@ int main()
         while(bL<bR)
         {
             int bM=bL+(bR-bL)/2;
-            qu_ans=1;
+            qu_low=1;
+            qu_upp=1;
             qu_L=L;
             qu_r=R+1;
             qu_x=num[bM];
             query(1,1,n+1);
-            if(qu_ans<=k)
-                bL=bM+1;
-            else bR=bM;
+            if(k>=qu_low && k<qu_upp)
+            {
+                bL=bM;
+                break;
+            }
+            else if(qu_low>k)
+                bR=bM-1;
+            else bL=bM+1;
         }
-        printf("%d\n",num[bL-1]);
+        printf("%d\n",num[bL]);
     }
     return 0;
 }
