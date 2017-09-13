fb2ee8cbcd78ae04d1338d8f18ac27a6b3c38a77
Yue Zhihan <zhihan.yue@foxmail.com>
2017-09-13 14:20:42 +0800

    RE

--- 2104.cpp
+++ 2104.cpp
@@ -8,14 +8,20 @@ int A[MAXN],B[MAXN];
 struct Node {
     int cnt;
     Node *left,*right;
+    Node(){}
     Node(int _cnt,Node *_left,Node *_right):cnt(_cnt),left(_left),right(_right){}
-};
+} nodes[10*MAXN];
+int nodes_k=0;
+Node *new_Node(int _cnt,Node *_left,Node *_right)
+{
+    return &(nodes[nodes_k++]=Node(_cnt,_left,_right));
+}
 
 void push_down(Node *o)
 {
     //Equivalent transformation
-    if(o->left==NULL) o->left=new Node(0,NULL,NULL);
-    if(o->right==NULL) o->right=new Node(0,NULL,NULL);
+    if(o->left==NULL) o->left=new_Node(0,NULL,NULL);
+    if(o->right==NULL) o->right=new_Node(0,NULL,NULL);
     //Non-equivalent transformation(null)
     
 }
@@ -28,10 +34,10 @@ void push_up(Node *o)
 Node *newver(Node *o,int L,int r,int newval)
 {
     if(newval<L || newval>=r) return o;
-    if(L+1==r){return new Node(o->cnt+1,NULL,NULL);}
+    if(L+1==r){return new_Node(o->cnt+1,NULL,NULL);}
     int M=L+(r-L)/2;
     push_down(o);
-    Node *newnode=new Node(0,newver(o->left,L,M,newval),newver(o->right,M,r,newval));
+    Node *newnode=new_Node(0,newver(o->left,L,M,newval),newver(o->right,M,r,newval));
     push_up(newnode);
     return newnode;
 }
@@ -55,7 +61,7 @@ int main()
     fromto(1,n,i) {scanf("%d",&A[i]);B[i]=A[i];}
     sort(B+1,B+n+1);
     fromto(1,n,i) A[i]=lower_bound(B+1,B+n+1,A[i])-B;
-    Version[0]=new Node(0,NULL,NULL);
+    Version[0]=new_Node(0,NULL,NULL);
     fromto(1,n,i) Version[i]=newver(Version[i-1],1,n+1,A[i]);
     while(m--) {
         int L,R,k;
