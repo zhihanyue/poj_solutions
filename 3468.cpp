73af0ad46bf6598a55a1002a1c1ec2e2d8daffa0
Yue Zhihan <zhihan.yue@foxmail.com>
2017-10-17 14:26:43 +0800

    AC 3641ms 560k

--- 3468.cpp
+++ 3468.cpp
@@ -1,38 +1,25 @@
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+//#define B 2
+#define B 300
 using namespace std;
 int A[100008],N;
+long long bucket[400],addv[400];
 
-int lowbit(int i)
+int num(int i)
 {
-    return i&(-i);
+    return (i-1)/B+1;
 }
-
-struct BIT
-{
-    long long bit[100008];
-    long long sum(int i)
-    {
-        long long result=0;
-        for(;i>0;i-=lowbit(i))
-            result+=bit[i];
-        return result;
-    }
-    void add(int i,int v)
-    {
-        for(;i<=N;i+=lowbit(i))
-            bit[i]+=v;
-    }
-}BIT1,BIT2;
 
 int main()
 {
     int Q;
     scanf("%d%d",&N,&Q);
     for(int i=1;i<=N;++i)
-    {
         scanf("%d",&A[i]);
-        BIT2.add(i,A[i]);
-    }
+    for(int i=1;i<=N;++i)
+        bucket[num(i)]+=A[i];
     for(int i=1;i<=Q;++i)
     {
         char ch;
@@ -41,12 +28,33 @@ int main()
         if(ch=='C')
         {
             scanf("%d",&x);
-            BIT1.add(a,x);
-            BIT1.add(b+1,-x);
-            BIT2.add(a,-(a-1)*x);
-            BIT2.add(b+1,b*x);
+            for(;a<=b && (a-1)%B!=0;++a)
+            {
+                A[a]+=x;
+                bucket[num(a)]+=x;
+            }
+            for(;a<=b && b%B!=0;--b)
+            {
+                A[b]+=x;
+                bucket[num(b)]+=x;
+            }
+            for(int j=a;j<=b;j+=B)
+            {
+                bucket[num(j)]+=x*B;
+                addv[num(j)]+=x;
+            }
         }
-        else printf("%I64d\n",(BIT1.sum(b)*b+BIT2.sum(b))-(BIT1.sum(a-1)*(a-1)+BIT2.sum(a-1)));
+        else
+        {
+            long long result=0;
+            for(;a<=b && (a-1)%B!=0;++a)
+                result+=A[a]+addv[num(a)];
+            for(;a<=b && b%B!=0;--b)
+                result+=A[b]+addv[num(b)];
+            for(int j=a;j<=b;j+=B)
+                result+=bucket[num(j)];
+            printf("%I64d\n",result);
+        }
     }
     return 0;
 }
