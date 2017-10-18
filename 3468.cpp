775f0c947e55d26b3a07d36a96c2f0362cdff92c
Yue Zhihan <zhihan.yue@foxmail.com>
2017-10-18 14:27:00 +0800

    RE

--- 3468.cpp
+++ 3468.cpp
@@ -1,15 +1,70 @@
 #include <cstdio>
 #include <cstdlib>
-#include <cstring>
-//#define B 2
-#define B 300
 using namespace std;
 int A[100008],N;
-long long bucket[400],addv[400];
+long long xdtree[200008],addv[200008];
 
-int num(int i)
+void init(int o,int L,int r)
 {
-    return (i-1)/B+1;
+    addv[o]=0;
+    if(L+1==r)
+    {
+        xdtree[o]=A[L];
+        return;
+    }
+    int M=L+(r-L)/2;
+    init(o*2,L,M);
+    init(o*2+1,M,r);
+    xdtree[o]=xdtree[o*2]+xdtree[o*2+1];
+}
+
+void pushdown(int o,int L,int r)
+{
+    if(L+1!=r)
+    {
+        addv[o*2]+=addv[o];
+        addv[o*2+1]+=addv[o];
+        int M=L+(r-L)/2;
+        xdtree[o*2]+=(M-L)*addv[o];
+        xdtree[o*2+1]+=(r-M)*addv[o];
+    }
+    addv[o]=0;
+}
+
+int qu_L,qu_r;
+long long qu_ans;
+void query(int o,int L,int r)
+{
+    if(L>=qu_r || r<=qu_L)
+        return;
+    if(qu_L<=L && r<=qu_r)
+    {
+        qu_ans+=xdtree[o];
+        return;
+    }
+    pushdown(o,L,r);
+    int M=L+(r-L)/2;
+    query(o*2,L,M);
+    query(o*2+1,M,r);
+}
+
+int up_L,up_r;
+long long up_add;
+void update(int o,int L,int r)
+{
+    if(L>=up_r || r<=up_L)
+        return;
+    if(up_L<=L && r<=up_r)
+    {
+        addv[o]+=up_add;
+        xdtree[o]+=(r-L)*up_add;
+        return;
+    }
+    pushdown(o,L,r);
+    int M=L+(r-L)/2;
+    update(o*2,L,M);
+    update(o*2+1,M,r);
+    xdtree[o]=xdtree[o*2]+xdtree[o*2+1];
 }
 
 int main()
@@ -18,8 +73,7 @@ int main()
     scanf("%d%d",&N,&Q);
     for(int i=1;i<=N;++i)
         scanf("%d",&A[i]);
-    for(int i=1;i<=N;++i)
-        bucket[num(i)]+=A[i];
+    init(1,1,N+1);
     for(int i=1;i<=Q;++i)
     {
         char ch;
@@ -28,32 +82,18 @@ int main()
         if(ch=='C')
         {
             scanf("%d",&x);
-            for(;a<=b && (a-1)%B!=0;++a)
-            {
-                A[a]+=x;
-                bucket[num(a)]+=x;
-            }
-            for(;a<=b && b%B!=0;--b)
-            {
-                A[b]+=x;
-                bucket[num(b)]+=x;
-            }
-            for(int j=a;j<=b;j+=B)
-            {
-                bucket[num(j)]+=x*B;
-                addv[num(j)]+=x;
-            }
+            up_L=a;
+            up_r=b+1;
+            up_add=x;
+            update(1,1,N+1);
         }
         else
         {
-            long long result=0;
-            for(;a<=b && (a-1)%B!=0;++a)
-                result+=A[a]+addv[num(a)];
-            for(;a<=b && b%B!=0;--b)
-                result+=A[b]+addv[num(b)];
-            for(int j=a;j<=b;j+=B)
-                result+=bucket[num(j)];
-            printf("%I64d\n",result);
+            qu_L=a;
+            qu_r=b+1;
+            qu_ans=0;
+            query(1,1,N+1);
+            printf("%I64d\n",qu_ans);
         }
     }
     return 0;
