be3a389743b6676563391f0cb9317a4400f42d8c
Yue Zhihan <zhihan.yue@foxmail.com>
2017-10-19 14:28:05 +0800

    WA

--- 3468.cpp
+++ 3468.cpp
@@ -1,71 +1,34 @@
-#include <cstdio>
-#include <cstdlib>
+#include <iostream>
+#define lowbit(i) ((i)&(-(i)))
 using namespace std;
 int A[100008],N;
-long long xdtree[400008],addv[400008];
 
-void init(int o,int L,int r)
+struct BIT
 {
-    addv[o]=0;
-    if(L+1==r)
+    long long bit[100008],len;
+    void init(int A[],int _len)
     {
-        xdtree[o]=A[L];
-        return;
+        len=_len;
+        int S[100008];
+        S[0]=0;
+        for(int i=1;i<=len;++i)
+            S[i]=S[i-1]+A[i];
+        for(int i=1;i<=len;++i)
+            bit[i]=S[i]-S[i-lowbit(i)];
     }
-    int M=L+(r-L)/2;
-    init(o*2,L,M);
-    init(o*2+1,M,r);
-    xdtree[o]=xdtree[o*2]+xdtree[o*2+1];
-}
-
-void pushdown(int o,int L,int r)
-{
-    if(L+1!=r)
+    long long sum(int i)
     {
-        addv[o*2]+=addv[o];
-        addv[o*2+1]+=addv[o];
-        int M=L+(r-L)/2;
-        xdtree[o*2]+=(M-L)*addv[o];
-        xdtree[o*2+1]+=(r-M)*addv[o];
+        long long result=0;
+        for(;i>0;i-=lowbit(i))
+            result+=bit[i];
+        return result;
     }
-    addv[o]=0;
-}
-
-int qu_L,qu_r;
-long long qu_ans;
-void query(int o,int L,int r)
-{
-    if(L>=qu_r || r<=qu_L)
-        return;
-    if(qu_L<=L && r<=qu_r)
+    void add(int i,int v)
     {
-        qu_ans+=xdtree[o];
-        return;
+        for(;i<=len;i+=lowbit(i))
+            bit[i]+=v;
     }
-    pushdown(o,L,r);
-    int M=L+(r-L)/2;
-    query(o*2,L,M);
-    query(o*2+1,M,r);
-}
-
-int up_L,up_r;
-long long up_add;
-void update(int o,int L,int r)
-{
-    if(L>=up_r || r<=up_L)
-        return;
-    if(up_L<=L && r<=up_r)
-    {
-        addv[o]+=up_add;
-        xdtree[o]+=(r-L)*up_add;
-        return;
-    }
-    pushdown(o,L,r);
-    int M=L+(r-L)/2;
-    update(o*2,L,M);
-    update(o*2+1,M,r);
-    xdtree[o]=xdtree[o*2]+xdtree[o*2+1];
-}
+}BIT1,BIT2;
 
 int main()
 {
@@ -73,7 +36,8 @@ int main()
     scanf("%d%d",&N,&Q);
     for(int i=1;i<=N;++i)
         scanf("%d",&A[i]);
-    init(1,1,N+1);
+    BIT1.len=N;
+    BIT2.init(A,N);
     for(int i=1;i<=Q;++i)
     {
         char ch;
@@ -82,19 +46,12 @@ int main()
         if(ch=='C')
         {
             scanf("%d",&x);
-            up_L=a;
-            up_r=b+1;
-            up_add=x;
-            update(1,1,N+1);
+            BIT1.add(a,x);
+            BIT1.add(b+1,-x);
+            BIT2.add(a,-(a-1)*x);
+            BIT2.add(b+1,b*x);
         }
-        else
-        {
-            qu_L=a;
-            qu_r=b+1;
-            qu_ans=0;
-            query(1,1,N+1);
-            printf("%I64d\n",qu_ans);
-        }
+        else printf("%I64d\n",(BIT1.sum(b)*b+BIT2.sum(b))-(BIT1.sum(a-1)*(a-1)+BIT2.sum(a-1)));
     }
     return 0;
 }
