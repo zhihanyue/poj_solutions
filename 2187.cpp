bf1ab1c466d9ed48984203b8b48d9c765dc72292
Yue Zhihan <zhihan.yue@foxmail.com>
2017-10-24 14:29:21 +0800

    CE

--- 2187.cpp
+++ 2187.cpp
@@ -29,11 +29,11 @@ int main()
     sort(P+1,P+n+1);
     int k=0;
     for(int i=1;i<=n;++i) {
-        while(k>=2 && cross(TB[k]-TB[k-1],P[i]-TB[k])<=0) --k;
+        for(;k>=2 && cross(TB[k]-TB[k-1],P[i]-TB[k])<=0;--k);
         TB[++k]=P[i];
     }
     for(int i=n-1,t=k;i>=1;--i) {
-        while(k>=t+1 && cross(TB[k]-TB[k-1],P[i]-TB[k])<=0) --k;
+        for(;k>=t+1 && cross(TB[k]-TB[k-1],P[i]-TB[k])<=0;--k);
         TB[++k]=P[i];
     }
     //凸包免费赠送TB[k]这个哨兵哦~~ 
@@ -43,6 +43,6 @@ int main()
         if(area2(TB[i],TB[i+1],TB[j+1])>area2(TB[i],TB[i+1],TB[j])) j=j%k+1;
         ans=max(ans,max(len2(TB[j]-TB[i]),len2(TB[j+1]-TB[i+1])));
     }
-    printf("%.0lf\n",ans);
+    printf("%.0lf\n",round(ans));
     return 0;
 }
