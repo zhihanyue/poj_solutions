1f5aa9bd51a933773b4c2fdada3ef6355c1eff49
Yue Zhihan <zhihan.yue@foxmail.com>
2017-11-10 14:36:22 +0800

    WA

---
+++ 3684.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#define g 10.0
+using namespace std;
+int N,H,R;
+double y[108];
+
+double f(int T)
+{
+    if(T<=0)
+        return H;
+    double t=sqrt(2*H/g);
+    int n=T/t;
+    double k=T-n*t,y;
+    if(n%2==0)
+        return H-k*k*g/2;
+    else return k*k*g/2;
+}
+
+
+int main()
+{
+    int casecnt;
+    scanf("%d",&casecnt);
+    while(casecnt--)
+    {
+        int T;
+        scanf("%d%d%d%d",&N,&H,&R,&T);
+        for(int i=0;i<N;++i)
+            y[i]=f(T-i);
+        sort(y,y+N);
+        for(int i=0;i<N;++i)
+            if(i==N-1)
+                printf("%.2lf\n",y[i]+2*R*i/100.0);
+            else printf("%.2lf ",y[i]+2*R*i/100.0);
+    }
+    //system("pause");
+    return 0;
+}
