a29f6c71833cb6475747002b140f15ea32257ef9
Yue Zhihan <zhihan.yue@foxmail.com>
2017-11-16 14:38:25 +0800

    AC 204MS 280K

--- 1734.cpp
+++ 1734.cpp
@@ -1,44 +1,68 @@
-#include <cstdio>
-#include <climits>
-#include <algorithm>
+#include<stdio.h>
+#include<vector>
+#include<string.h>
 using namespace std;
-#define MAXN 108
-#define INF (INT_MAX/2)
-#define fromto(from,to,i) for(int i=(from);i<=(to);++i)
+const int N = 110;
+const int inf = 1e8;
+struct node{
+    int to,w;
+};
+vector<node> QQ[N];
+bool vis[N];
+int S,ans[N],temp[N],best,ans_nodes;
+void dfs(int now,int fa,int dep,int sum)
+{
+     int i;
+     if(now==S&&vis[S])
+     {
+         if(sum<best)
+         {
+             best=sum;ans_nodes=dep;
+             for(i=0;i<dep;i++)
+                 ans[i]=temp[i];
+         }
+         return ;
+     }
+     temp[dep]=now;
+     for(i=0;i<QQ[now].size();i++)
+     {
+         if(QQ[now][i].to!=S&&vis[QQ[now][i].to]) continue;
+         if(QQ[now][i].to==fa) continue;
+         vis[QQ[now][i].to]=true;
+         if(QQ[now][i].w+sum<best) 
+             dfs(QQ[now][i].to,now,dep+1,sum+QQ[now][i].w);
+         vis[QQ[now][i].to]=false;
+     }
+}
 int main()
 {
-    static int G[MAXN][MAXN],d[MAXN][MAXN],pre[MAXN][MAXN];
     int n,m;
+    int i,w,s,t;
     scanf("%d%d",&n,&m);
-    fromto(1,n,i) fromto(1,n,j) {
-        G[i][j]=INF;
-        d[i][j]=INF;
-        pre[i][j]=i;
-    }
-    fromto(1,m,i) {
-        int u,v,w;
-        scanf("%d%d%d",&u,&v,&w);
-        w=min(w,G[u][v]);
-        G[u][v]=w;G[v][u]=w;
-        d[u][v]=w;d[v][u]=w;
-    }
-    static int minhuan=INF,path[MAXN],path_len;
-    fromto(1,n,k) {
-        fromto(1,k-1,i) fromto(i+1,k-1,j)
-            if(d[i][j]!=INF && G[i][k]!=INF && G[k][j]!=INF && minhuan>d[i][j]+G[i][k]+G[k][j]) {
-                minhuan=d[i][j]+G[i][k]+G[k][j];
-                path_len=0;
-                for(int v=j;v!=i;v=pre[i][v]) path[++path_len]=v;
-                path[++path_len]=i; path[++path_len]=k;
-            }
-        fromto(1,n,i) fromto(1,n,j)
-            if(d[i][k]!=INF && d[k][j]!=INF && d[i][j]>d[i][k]+d[k][j]) {
-                d[i][j]=d[i][k]+d[k][j];
-                pre[i][j]=pre[k][j];
-            }
-    }
-    if(minhuan==INF){printf("No solution.\n");return 0;}
-    fromto(1,path_len,i) printf(i!=path_len?"%d ":"%d\n",path[i]);
+        node tmp;
+        for(i=0;i<m;i++)
+        {
+            scanf("%d%d%d",&s,&t,&w);
+            tmp.to=t;
+            tmp.w=w;
+            QQ[s].push_back(tmp);
+            tmp.to=s;
+            QQ[t].push_back(tmp);
+        }
+        best=inf;//错误原因：全局变量在这里又定义了一遍，汗！！！！
+        for(i=1;i<=n;i++)
+        {
+            memset(vis,false,sizeof(vis));
+            S=i;
+            dfs(i,-1,0,0);
+        }
+        if(best==inf) printf("No solution.\n");
+        else 
+        {
+            printf("%d",ans[0]);
+            for(i=1;i<ans_nodes;i++)
+                printf(" %d",ans[i]);
+        }
+        printf("\n");
     return 0;
 }
- 
